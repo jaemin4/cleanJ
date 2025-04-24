7e6c970ab05d4723786df4100e1dd43a4f99f2e0
jaemin4 <eheh25877@gmail.com>
2025-04-25 00:35:47 +0900

    fix: 재고감소 : PESSIMISTIC_WRITE 락에 타임아웃 설정

--- src/main/java/com/example/demo/infra/stock/StockJpaRepository.java
+++ src/main/java/com/example/demo/infra/stock/StockJpaRepository.java
@@ -2,12 +2,13 @@ package com.example.demo.infra.stock;
 
 import com.example.demo.domain.stock.Stock;
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
@@ -15,6 +16,7 @@ public interface StockJpaRepository extends JpaRepository<Stock, Long> {
     Optional<Stock> findByProductId(Long productId);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints(@QueryHint(name = "javax.persistence.lock.timeout", value = "1000"))
     @Query("SELECT s FROM Stock s WHERE s.productId = :productId")
     Optional<Stock> findWithPessimisticLock(@Param("productId") Long productId);
 
