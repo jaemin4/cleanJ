a7d7aef518ec6b91c5a34b573faceab299942f57
jaemin4 <eheh25877@gmail.com>
2025-04-25 00:34:06 +0900

    fix: PESSIMISTIC_WRITE 락에 타임아웃 설정

--- src/main/java/com/example/demo/infra/coupon/CouponJpaRepository.java
+++ src/main/java/com/example/demo/infra/coupon/CouponJpaRepository.java
@@ -2,16 +2,20 @@ package com.example.demo.infra.coupon;
 
 import com.example.demo.domain.coupon.Coupon;
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CouponJpaRepository extends JpaRepository<Coupon, Long> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints(@QueryHint(name = "javax.persistence.lock.timeout", value = "1000"))
     @Query("select c from Coupon c where c.id = :couponId")
-    Optional<Coupon> findByCouponIdForLock(Long couponId);
+    Optional<Coupon> findByCouponIdForLock(@Param("couponId") Long couponId);
 
 }
