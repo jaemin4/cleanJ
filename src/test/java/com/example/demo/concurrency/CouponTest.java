d1830ef41b7fb00288d097c27bc21b43173de402
jaemin4 <eheh25877@gmail.com>
2025-05-09 04:03:47 +0900

    fix : class 이름 변경

--- src/test/java/com/example/demo/concurrency/CouponTest.java
+++ src/test/java/com/example/demo/concurrency/CouponTest.java
@@ -1,7 +1,6 @@
 package com.example.demo.concurrency;
 
 import com.example.demo.domain.coupon.*;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,7 +10,7 @@ import java.util.concurrent.Executors;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
-public class CouponTestTest {
+public class CouponTest {
 
     @Autowired
     private CouponService couponService;
