92c8005b17dfce190ba13e8a34d78e2840ee1175
jaemin4 <eheh25877@gmail.com>
2025-05-16 04:32:39 +0900

    fix : 기존 테스트 코드 삭제

--- src/test/java/com/example/demo/concurrency/CouponTest.java
+++ src/test/java/com/example/demo/concurrency/CouponTest.java
@@ -30,44 +30,6 @@ public class CouponTest {
     @Autowired
     private CouponScheduler couponScheduler;
 
-
-    @Test
-    public void 쿠폰_200명_발급_테스트_동시성_성공_확인() throws InterruptedException {
-        Coupon coupon = Coupon.create("테스트 쿠폰", 100, 100L);
-        Coupon saved = couponRepository.save(coupon);
-
-        Long couponId = saved.getId();
-        System.out.println("[Init] 쿠폰 수량 = " + coupon.getQuantity());
-
-        int userCount = 200;
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
-        CountDownLatch latch = new CountDownLatch(userCount);
-
-        for (long userId = 1; userId <= userCount; userId++) {
-            final long finalUserId = userId;
-            executorService.execute(() -> {
-                try {
-                    couponService.issue(CouponCommand.Issue.of(finalUserId, couponId));
-                    System.out.println("[SUCCESS] userId=" + finalUserId);
-                } catch (Exception e) {
-                    System.out.println("[FAIL] userId=" + finalUserId + " => " + e.getMessage());
-                } finally {
-                    latch.countDown();
-                }
-            });
-        }
-
-        latch.await();
-
-        long remaining = couponRepository.findById(couponId).get().getQuantity();
-        long issuedCount = userCouponRepository.count();
-
-        System.out.println("[Result] 남은 수량: " + remaining);
-        System.out.println("[Result] 발급된 유저 수: " + issuedCount);
-
-        assertThat(issuedCount).isEqualTo(100L);
-    }
-
     @Test
     public void 쿠폰_200명_발급_테스트_동시성_성공_비동기() throws InterruptedException {
         Coupon coupon = Coupon.create("테스트 쿠폰", 100, 100L);
