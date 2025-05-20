8c04ac76776c8d559bfca59b9a0b63a415a91c1b
jaemin4 <eheh25877@gmail.com>
2025-05-20 10:37:30 +0900

    fix: 비동기 트랜잭션 저장 로직을 트랜잭션 외부로 분리

--- src/main/java/com/example/demo/application/payment/PaymentFacade.java
+++ src/main/java/com/example/demo/application/payment/PaymentFacade.java
@@ -3,7 +3,9 @@ package com.example.demo.application.payment;
 import com.example.demo.support.comm.aop.DistributedLock;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
+import static com.example.demo.support.constants.RabbitmqConstant.*;
 
 @Slf4j
 @Component
@@ -11,10 +13,24 @@ import org.springframework.stereotype.Component;
 public class PaymentFacade {
 
     private final PaymentTransaction paymentTransaction;
+    private final RabbitTemplate rabbitTemplate;
 
     @DistributedLock(key = "'payment:user:' + #criteria.userId", waitTime = 3, leaseTime = 5)
     public void pay(PaymentCriteria.Payment criteria) {
-        paymentTransaction.processPaymentWithTransaction(criteria);
+        PaymentTransactionResult.Payment result = paymentTransaction.processPaymentWithTransaction(criteria);
+
+        //결제내역 저장
+        rabbitTemplate.convertAndSend(
+                EXCHANGE_PAYMENT_HISTORY, ROUTE_PAYMENT_HISTORY_DB_SAVE,
+                criteria.toPaymentHistoryConsumerCommand(result.getFinalAmount(), result.getTransactionId(), result.getStatus())
+        );
+
+        //Redis 랭킹 업데이트
+        rabbitTemplate.convertAndSend(
+                EXCHANGE_PAYMENT_HISTORY, ROUTE_PAYMENT_HISTORY_REDIS_UPDATE,
+                criteria.getOrderId()
+        );
+
     }
 
 
--- src/main/java/com/example/demo/application/payment/PaymentTransaction.java
+++ src/main/java/com/example/demo/application/payment/PaymentTransaction.java
@@ -12,8 +12,10 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-import static com.example.demo.support.constants.RabbitmqConstant.*;
+
+import static com.example.demo.infra.payment.PaymentPopularScheduler.POPULAR_PRODUCTS_KEY;
 
 @Slf4j
 @Service
@@ -24,64 +26,50 @@ public class PaymentTransaction {
     private final MockPaymentService mockPaymentService;
     private final CouponService couponService;
     private final StockService stockService;
-    private final RabbitTemplate rabbitTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Transactional
-    public void processPaymentWithTransaction(PaymentCriteria.Payment criteria) {
+    public PaymentTransactionResult.Payment processPaymentWithTransaction(PaymentCriteria.Payment criteria) {
         OrderInfo.GetOrder order = null;
         try {
+
+            //주문 조회
             order = orderService.getOrderById(criteria.getOrderId());
             long finalAmount = order.getProductTotalPrice();
+
             if (order.getOrderStatus().equals(OrderStatus.PAID)) {
                 log.info("이미 결제된 주문입니다.");
                 throw new RuntimeException("이미 결제된 주문입니다.");
             }
-            /*
-                1. 쿠폰사용
-            */
+
+            //쿠폰사용
             if (criteria.getCouponId() != null) {
                 couponService.use(criteria.toUseCouponCommand());
                 finalAmount = couponService.calculateDiscountedAmount(finalAmount, criteria.getCouponId());
             }
 
-            /*
-                2. 포인트사용
-            */
+            // 포인트사용
             balanceService.use(criteria.toBalanceUseCommand(finalAmount));
 
-            /*
-                3. 주문상태변경
-            */
+            //주문상태변경
             orderService.updateOrderStatus(criteria.getOrderId(), OrderStatus.PAID);
 
-            /*
-                4. 외부 API 호출
-            */
+            //외부 API 호출
             PaymentMockResponse.MockPay mockPaymentResponse = mockPaymentService.callAndValidateMockApi(
-                    criteria.toPaymentMockRequest(order.getProductTotalPrice())
+                    criteria.toPaymentMockRequest(finalAmount)
             );
 
+            //API 호출 검증
             if (!"SUCCESS".equals(mockPaymentResponse.getStatus())) {
                 log.error("결제 실패: orderId={}, status={}", criteria.getOrderId(), mockPaymentResponse.getStatus());
                 throw new RuntimeException("결제 API 실패");
             }
-            /*
-                5. 결제내역 저장
-            */
-            rabbitTemplate.convertAndSend(
-                    EXCHANGE_PAYMENT_HISTORY, ROUTE_PAYMENT_HISTORY_DB_SAVE,
-                    criteria.toPaymentHistoryConsumerCommand(finalAmount, mockPaymentResponse.getTransactionId(), mockPaymentResponse.getStatus())
+
+            return PaymentTransactionResult.Payment.of(
+                    mockPaymentResponse.getTransactionId(),
+                    mockPaymentResponse.getStatus(),
+                    finalAmount
             );
-
-            /*
-                6. Redis 랭킹 업데이트
-            */
-
-            rabbitTemplate.convertAndSend(
-                    EXCHANGE_PAYMENT_HISTORY, ROUTE_PAYMENT_HISTORY_REDIS_UPDATE,
-                    criteria.getOrderId()
-            );
-
 
         } catch (Exception e) {
             log.warn("트랜잭션 내 결제 실패, 주문 상태 취소 및 재고 복구 수행");
@@ -90,12 +78,15 @@ public class PaymentTransaction {
                 if (order != null && order.getOrderStatus().equals(OrderStatus.CREATED)) {
                     log.info("주문 상태 복구 및 재고 복구 시작: orderId={}", criteria.getOrderId());
 
-                    // 1. 주문 상태 변경
+                    //주문 상태 변경
                     orderService.updateOrderStatus(criteria.getOrderId(), OrderStatus.CANCELED);
 
-                    // 2. 재고 복구
+                    //재고 복구
                     OrderInfo.GetOrderItems getOrderItems = orderService.getOrderItemByOrderId(criteria.getOrderId());
                     stockService.recoveryStock(criteria.toRecoveryStockCommand(getOrderItems));
+
+                    //쿠폰 랭킹 복구
+                   // redisTemplate.opsForZSet().incrementScore(POPULAR_PRODUCTS_KEY, productId, -1);
 
                     log.info("주문 취소 및 재고 복구 완료");
                 } else {
