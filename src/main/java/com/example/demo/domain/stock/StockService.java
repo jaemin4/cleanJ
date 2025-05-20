4a5659108ea992e0772cb36299fe351f51a1cfd7
jaemin4 <eheh25877@gmail.com>
2025-05-20 18:14:35 +0900

    fix: 재고 감소 및 회복에 Redisson 분산락 적용 오류 수정

--- src/main/java/com/example/demo/domain/stock/StockService.java
+++ src/main/java/com/example/demo/domain/stock/StockService.java
@@ -1,22 +1,26 @@
 package com.example.demo.domain.stock;
 
 import com.example.demo.support.comm.aop.DistributedLock;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
 public class StockService {
     private final StockTransactionService stockTransactionService;
 
-    @DistributedLock(key = "'stock:deduct:' + #command.userId", waitTime = 5, leaseTime = 3)
+    @DistributedLock(key = "'lock:stock:deduct'", waitTime = 5, leaseTime = 3)
     public void deductStock(StockCommand.DeductStock command) {
         stockTransactionService.deductStockWithTransaction(command);
     }
 
-    @Transactional
-    public void recoveryStock(StockCommand.RecoveryStock command){
+    @DistributedLock(key = "'lock:stock:recovery'", waitTime = 5, leaseTime = 3)
+    public void recoveryStock(StockCommand.RecoveryStock command) {
         stockTransactionService.recoveryStockWithTransaction(command);
     }
+
+
+
+
 }
